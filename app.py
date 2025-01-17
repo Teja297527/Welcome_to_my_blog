f8a61bc595c52689d04aa5edd1cf704571f0a451
Teja297527 <nagabushanamvanarasa@gmail.com>
2025-01-17 20:33:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("HI, Welcome to our blog")
+st.title("PET VET")
+st.write("It is a good product for cows health")
+st.write("[Learn more]()")
